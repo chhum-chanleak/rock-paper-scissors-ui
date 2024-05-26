032e1203e86e5793ae892ee04eaaf6fc66702ae6
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-05-26 16:07:11 +0700

    fix: Update getComputerChoice funcction logic

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,17 +1,19 @@
 const getComputerChoice = () => {
 
-  let randomNum = Math.random().toFixed(2);
+  let randomNum = Math.floor(Math.random() * 3);
   let choice = "";
 
-  if (randomNum <= 0.33) {
+  if (randomNum === 1) {
     choice = "Rock";
     return choice;
-  } else if (randomNum > 0.33 && randomNum <= 0.66) {
+  } else if (randomNum === 2) {
     choice = "Paper";
     return choice;
   } else {
     choice = "Scissors";
     return choice;
   }
-   
 };
+
+
+
