18d3ff0bb6517a419ded1cb5ff288a3d651d36f6
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-05-26 12:11:00 +0700

    feat: Implement getComputChoice()

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,1 +1,17 @@
-console.log("Hello, world!1")
+const getComputerChoice = () => {
+
+  let randomNum = Math.random().toFixed(2);
+  let choice = "";
+
+  if (randomNum <= 0.33) {
+    choice = "Rock";
+    return choice;
+  } else if (randomNum > 0.33 && randomNum <= 0.66) {
+    choice = "Paper";
+    return choice;
+  } else {
+    choice = "Scissors";
+    return choice;
+  }
+   
+};
